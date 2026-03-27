e3a92353a9bfd18151447cf592b0c1ddf7ebc42c
asano69 <186772504+asano69@users.noreply.github.com>
2026-03-27 10:54:53 +0900

    新規ファイル作成する場合、Unix時間にする

--- src/command/command.ts
+++ src/command/command.ts
@@ -191,11 +191,7 @@ new (class New extends Command {
 	constructor() { super("new", "New map"); }
 	execute() {
 		if (!confirm("Throw away your current map and start a new one?")) { return; }
-		const now = new Date();
-		const yy = String(now.getFullYear()).slice(2);
-		const mm = String(now.getMonth() + 1).padStart(2, "0");
-		const dd = String(now.getDate()).padStart(2, "0");
-		const filename = `${yy}${mm}${dd}.mymind`;
+		const filename = `${Math.floor(Date.now() / 1000)}.mymind`;
 		window.history.pushState(null, "", `/m/${filename}`);
 		io.restore();
 	}
