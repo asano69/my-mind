4128708512afa98dcc60d60fccbf2e0bd528ae40
asano69 <186772504+asano69@users.noreply.github.com>
2026-03-28 17:36:38 +0900

    titleの設定の変更

--- src/title.ts
+++ src/title.ts
@@ -4,7 +4,7 @@ import { currentMap } from "./my-mind.js";
 
 function onItemChange(_message: string, publisher: any) {
 	if (publisher.isRoot && publisher.map == currentMap) {
-		document.title = currentMap.name + " :: My Mind";
+		document.title = currentMap.name + "";
 	}
 }
 
