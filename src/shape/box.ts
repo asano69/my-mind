5cded3e15bba118cc89e913149b00b15ed2bc5ad
asano69 <186772504+asano69@users.noreply.github.com>
2026-03-28 10:21:11 +0900

    ノードに着色する

--- src/shape/box.ts
+++ src/shape/box.ts
@@ -1,10 +1,11 @@
+// box.ts
 import Shape from "./shape.js";
-
-
+import Item from "../item.js";
 export default class Box extends Shape {
-	constructor() {
-		super("box", "Box");
-	}
+    constructor() { super("box", "Box"); }
+    update(item: Item) {
+        item.dom.content.style.setProperty('--item-color', item.resolvedColor);
+        item.dom.content.style.borderColor = ''; // CSS変数に委譲
+    }
 }
-
 new Box();
--- src/shape/ellipse.ts
+++ src/shape/ellipse.ts
@@ -1,10 +1,11 @@
+// ellipse.ts（同様）
 import Shape from "./shape.js";
-
-
+import Item from "../item.js";
 export default class Ellipse extends Shape {
-	constructor() {
-		super("ellipse", "Ellipse");
-	}
+    constructor() { super("ellipse", "Ellipse"); }
+    update(item: Item) {
+        item.dom.content.style.setProperty('--item-color', item.resolvedColor);
+        item.dom.content.style.borderColor = '';
+    }
 }
-
 new Ellipse();
