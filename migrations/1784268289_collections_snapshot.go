a7f40e9b085907cb041789243c45893d7c351918
asano69 <186772504+asano69@users.noreply.github.com>
2026-07-17 15:04:54 +0900

    chore

--- migrations/1784268289_collections_snapshot.go
+++ migrations/1784268289_collections_snapshot.go
@@ -1017,6 +1017,16 @@ func init() {
 						"presentable": false,
 						"system": false,
 						"type": "autodate"
+					},
+					{
+						"hidden": false,
+						"id": "autodate3342387181",
+						"name": "updated",
+						"onCreate": true,
+						"onUpdate": true,
+						"presentable": false,
+						"system": false,
+						"type": "autodate"
 					}
 				],
 				"id": "pbc_700096677",
