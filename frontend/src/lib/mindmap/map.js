f4926eac9bdda806c45169d02929954e86daaba4
asano69 <186772504+asano69@users.noreply.github.com>
2026-07-17 13:00:14 +0900

    date

--- frontend/src/lib/mindmap/map.js
+++ frontend/src/lib/mindmap/map.js
@@ -52,12 +52,11 @@ export default class Map {
       {
         root: (() => {
           const d = new Date();
-          const yy = String(d.getFullYear()).slice(-2);
+          const yyyy = d.getFullYear();
           const mm = String(d.getMonth() + 1).padStart(2, "0");
           const dd = String(d.getDate()).padStart(2, "0");
-          const hh = String(d.getHours()).padStart(2, "0");
-          const mi = String(d.getMinutes()).padStart(2, "0");
-          return `${yy}${mm}${dd}`;
+
+          return `${yyyy}-${mm}-${dd}`;
         })(),
         layout: layoutRepo.get("map"),
       },
