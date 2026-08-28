c078c6422834a0e4d154e7189c1b22d78ba10591
asano69 <186772504+asano69@users.noreply.github.com>
2026-08-28 20:49:27 +0900

    fix

--- frontend/src/lib/mindmap/ui/io.js
+++ frontend/src/lib/mindmap/ui/io.js
@@ -1,7 +1,7 @@
 import { createRoot, createEffect, on } from "solid-js";
 import * as backend from "../backend/pocketbase.js";
 import { serializeCurrentMap } from "../backend/image.js";
-import ItemNode from "../core/itemStore.js";
+import ItemNode from "mindmap-engine/itemStore.js";
 import {
   currentTitle,
   setCurrentTitle,
